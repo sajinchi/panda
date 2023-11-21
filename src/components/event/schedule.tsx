b700381c1eed314300fe5c488441369ef1e545c3
Sajin Chitrakar <xazinchitrakar@gmail.com>
2023-11-21 04:26:12 +0000

    schedule day working fix

--- src/components/event/schedule.tsx
+++ src/components/event/schedule.tsx
@@ -1,20 +1,10 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
-interface ActTypes {
-  image: string;
-  time: string;
-  act_type: string;
-  artist: string;
-}
-
-interface ScheduleTypes {
-  day: ActTypes[];
-}
 const Schedule = () => {
-  const actdata: ScheduleTypes = {
-    day: [
+  const data = [
+    [
       {
         image: "/artist1.png",
         time: "10:00 AM - 12:00 PM",
@@ -39,6 +29,8 @@ const Schedule = () => {
         act_type: "Opening Act",
         artist: "Trishna Gurung",
       },
+    ],
+    [
       {
         image: "/artist2.png",
         time: "8:00 PM - 12:00 PM",
@@ -58,21 +50,72 @@ const Schedule = () => {
         artist: "Trishna Gurung",
       },
     ],
-  };
-  const days = ["Day 1","Day 2","Day 3","Day 4","Day 5" ];
+    [
+      {
+        image: "/artist1.png",
+        time: "10:00 AM - 12:00 PM",
+        act_type: "Opening Act",
+        artist: "Trishna Gurung",
+      },
+      {
+        image: "/artist2.png",
+        time: "10:00 PM - 11:00 PM",
+        act_type: "Opening Act",
+        artist: "Trishna Gurung",
+      },
+      {
+        image: "/artist3.png",
+        time: "8:00 PM - 12:00 PM",
+        act_type: "Opening Act",
+        artist: "Trishna Gurung",
+      },
+      {
+        image: "/artist1.png",
+        time: "8:00 PM - 12:00 PM",
+        act_type: "Opening Act",
+        artist: "Trishna Gurung",
+      },
+    ],
+    [
+      {
+        image: "/artist2.png",
+        time: "8:00 PM - 12:00 PM",
+        act_type: "Opening Act",
+        artist: "Trishna Gurung",
+      },
+      {
+        image: "/artist3.png",
+        time: "8:00 PM - 12:00 PM",
+        act_type: "Opening Act",
+        artist: "Trishna Gurung",
+      },
+    ],
+  ];
 
+  const days = [];
+  for (let index = 1; index <= data.length; index++) {
+    days.push("Day " + index);
+  }
+  const [selectedDay, setSelectedDay] = useState<number>(0);
+  console.log(selectedDay);
   return (
     <section className="m-10 my-20 flex flex-col space-y-5" id="schedule">
       <div className="font-ChangaOne text-3xl text-white my-5">Schedule</div>
       <div className="flex text-[#D6D7FF] font-Amaranath font-bold text-xl space-x-2">
-        {days.map((day,index)=>{
-            return(
-                <button key= {index} className="flex w-[104px] h-9 items-center justify-center rounded-full hover:bg-white hover:text-black">{day}</button>
-            )
+        {days.map((day, index) => {
+          return (
+            <button
+              key={index}
+              className={`flex w-[104px] h-9 items-center justify-center rounded-full hover:bg-white hover:text-black ${index == selectedDay ? 'bg-white text-black' : ''} `}
+              onClick={() => setSelectedDay(index)}
+            >
+              {day}
+            </button>
+          );
         })}
-        </div>
-      <div className="flex flex-wrap bg-[#252A6C]  rounded-lg justify-between py-5">
-        {actdata.day.map((day, index) => {
+      </div>
+      <div className="flex flex-wrap bg-[#252A6C]  rounded-lg  py-5">
+        {data[selectedDay].map((day, index) => {
           const itemsPerRow = 3;
           const rowNumber = Math.floor(index / itemsPerRow);
 
@@ -85,7 +128,9 @@ const Schedule = () => {
               <div className="flex flex-col ">
                 <span className=" text-sm text-[#D6D7FF]">{day.time}</span>
                 <span className="text-xl text-white">{day.act_type}</span>
-                <span className="text-base text-[#FFF974] italic">{day.artist}</span>
+                <span className="text-base text-[#FFF974] italic">
+                  {day.artist}
+                </span>
               </div>
             </div>
           );
